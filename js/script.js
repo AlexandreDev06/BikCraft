9bb6d2abef16f56bfa542e1b1d7d4eb91a06ce00
alexandre <alexandre.macedo1712@gmail.com>
2021-04-21 13:05:11 -0300

    V10 Verificação no JavaScript

--- js/script.js
+++ js/script.js
@@ -26,3 +26,44 @@ if(window.SimpleForm) {
 	  sucesso: "<div id='form-sucesso'><h2>Formulário enviado com sucesso</h2><p>Em breve eu entro em contato com você.</p></div>", // mensagem de sucesso
 	});
 }
+
+// Mostrar e Ocultar
+
+var btn = document.querySelector('#modelos');
+
+var categoria = document.querySelector('.categoria');
+
+btn.addEventListener('click', function(){
+
+if(categoria.style.display === 'block'){
+	categoria.style.display = 'none';
+} else{
+	categoria.style.display = 'block';
+}
+});
+
+var btn2 = document.querySelector('#modelos2');
+
+var categoria2 = document.querySelector('.categoria2');
+
+btn2.addEventListener('click', function(){
+	
+if(categoria2.style.display === 'block'){
+	categoria2.style.display = 'none';
+} else{
+	categoria2.style.display = 'block';
+}
+});
+
+var btn3 = document.querySelector('#modelos3');
+
+var categoria3 = document.querySelector('.categoria3');
+
+btn3.addEventListener('click', function(){
+	
+if(categoria3.style.display === 'block'){
+	categoria3.style.display = 'none';
+} else{
+	categoria3.style.display = 'block';
+}
+});
